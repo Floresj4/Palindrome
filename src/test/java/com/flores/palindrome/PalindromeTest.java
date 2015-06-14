fa66b9bbfde77b118351b5f80336836fe8725b04
unknown <A2JAGZZ@W0155649.usac.mmm.com>
2015-06-14 19:25:27 -0400

    Adding a couple more test strings

--- src/test/java/com/flores/palindrome/PalindromeTest.java
+++ src/test/java/com/flores/palindrome/PalindromeTest.java
@@ -10,7 +10,11 @@ public class PalindromeTest {
 		assertTrue(Palindrome.test("radar"));
 		assertFalse(Palindrome.test("abaaansdn"));
 		assertTrue(Palindrome.test("tacocat"));
+		assertFalse(Palindrome.test("sd.lcjnvliar;ovar"));
+		assertTrue(Palindrome.test("Mr. Owl ate my metal worm"));
 		assertFalse(Palindrome.test("ra"));
 		assertTrue(Palindrome.test("Eva, can I stab bats in a cave?"));
+		assertFalse(Palindrome.test("qwerty"));
+		assertTrue(Palindrome.test("A Toyota's a Toyota"));
 	}
 }
